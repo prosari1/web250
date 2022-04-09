2cd90c4ca11f0c7d9a527d792b198ac6fcbe397f
prosari1 <59841362+prosari1@users.noreply.github.com>
2022-04-09 15:35:56 -0400

    Delete joy_of_php_source-2 directory

--- joy_of_php_source-2/joyphp/src/countdown.php
+++
@@ -1,7 +0,0 @@
-<?php
-$target = mktime(0, 0, 0, 12, 10, 2018) ;
-$today = time () ;
-$difference =($target-$today) ;
-$days =(int) ($difference/86400) ;
-print "Our event will occur in $days days";
-?>
